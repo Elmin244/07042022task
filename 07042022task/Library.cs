4b6eebd7ed67a68943b2cea7fd41266039f72214
unknown <tu7d5w0m5@code.edu.az>
2022-04-06 23:50:05 -0800

    first commit

---
+++ 07042022task/Library.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07042022task
+{
+    internal class Library
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Book> _BookList;
+        public Library()
+        {
+            _BookList = new List<Book>();
+        }
+        public void AddBook(Book book)
+        {
+            _BookList.Add(book);
+        }
+        public Book GetBookById(int id)
+        {
+            return _BookList.Find(x => x.Id == x.Id);
+        }
+        public void RemoveBook(int id)
+        {
+            var book = _BookList.Find(x => x.Id== id);
+            _BookList.Remove(book);
+
+        }
+
+    }
+}
---
+++ 07042022task/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+
+namespace _07042022task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Library library = new Library()
+            {
+                Name = "Elmin",
+                Id = 1,
+            };
+            string path = @"C:\Users\Lenovo\Desktop\homework\07042022";
+            Console.WriteLine("File name daxil edin :");
+            string fileName = Console.ReadLine();
+            if (!Directory.Exists(path + fileName))
+            {
+                Directory.CreateDirectory(path + fileName);
+            }
+            else
+            {
+                Console.WriteLine($"{fileName} adli file var");
+            }
+            if (!File.Exists(path + fileName + @"\Database.json"))
+            {
+                var createFile = File.Create(path + fileName + @"\Database.json");
+                createFile.Close();
+            }
+            else
+            {
+                Console.WriteLine("Database.json  adli file var");
+            }
+            bool menu = true;
+            while (menu)
+            {
+                Console.WriteLine("Menu :");
+                Console.WriteLine("1. Add book");
+                Console.WriteLine("2. Get book by id");
+                Console.WriteLine("3. Remove book");
+                Console.WriteLine("0. Quit");
+                Console.WriteLine("Secim Edin :");
+                string command = Console.ReadLine();
+                switch (command)
+                {
+                    case "0":
+                        menu = false;
+                        break;
+                    case "1":
+                        int bookId = IntInput("Book Id daxil din :", "Book Id duzgun daxil edin :");
+                        string bookName = StringInput("Book Name daxil edin :");
+                        string bookAuthorName = StringInput("Book AuthorName Daxil Edin :");
+                        double bookPrice = DoubleInput("Book Price daxil edin :", "Book Price duzgun daxil edin :");
+                        Book book = new Book()
+                        {
+                            Id = bookId,
+                            Name = bookName,
+                            AuthorName = bookAuthorName,
+                            Price = bookPrice,
+                        };
+                        library.AddBook(book);
+
+
+
+
+
+                }
+    }  
+}
